ec4f893e0c724ef039ecfed45df289251267351e
hfjgjfg <mohammadhaselmehri20202020@gmail.com>
2016-02-11 14:21:13 +0330

    Create version.lua

---
+++ plugins/version.lua
@@ -0,0 +1,17 @@
+do
+
+function run(msg, matches)
+  return 'robot v.1'.. [[ 
+ورژن ربات: 1.5
+ایدی سودو:
+@mohammad20162015
+end
+return {
+  description = "Shows bot version", 
+  usage = "!version: Shows infernal robot version",
+  patterns = {
+    "^!پرشین گارد$"
+  }, 
+  run = run 
+}
+end
